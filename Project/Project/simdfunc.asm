173dad0a316b9a85c303485f5029bbfccae9fda8
DDGC7 <99391788+DDGC7@users.noreply.github.com>
2023-07-17 06:25:56 +0800

    Update simdfunc.asm
    working now

--- Project/Project/simdfunc.asm
+++ Project/Project/simdfunc.asm
@@ -1,40 +1,27 @@
 section .text
-default rel
 bits 64
+default rel
 global dotProduct_SIMD
 
 dotProduct_SIMD:
-   push     rbp
-   mov      rbp, rsp
-   sub      rsp, 8
-   mov      r12, rdx
-   mov      r13, r8
-   xor      rax, rax
-   xor      ecx, ecx
-   mov      eax, ecx
-   shr      eax, 4             
-   vpxor    ymm2, ymm2, ymm2  
+    mov r12, rdx
+    mov r13, r8
+    shr rcx, 0       ; Update the shift value to divide by 4 (number of elements in each iteration)
+    xor rax, rax
 
-   dotProduct_loop:
-        cmp      eax, 0
-        jle      dotProduct_exit
-        vmovdqu  ymm0, [r12]    
-        vmovdqu  ymm1, [r13]    
-        vpmulld  ymm0, ymm0, ymm1 
-        vpaddd   ymm2, ymm2, ymm0 
-        add      r12, 32         
-        add      r13, 32        
-        sub      eax, 1         
-        jmp      dotProduct_loop
+dotProduct_loop:
+    cmp rcx, 0
+    jle dotProduct_exit
+    vmovq xmm0, qword [r12]    ; Load 64 bits (1 element) from [r12] into xmm0
+    vmovq xmm1, qword [r13]    ; Load 64 bits (1 element) from [r13] into xmm1
+    vpmuludq xmm0, xmm0, xmm1  ; Multiply xmm0 and xmm1
+    vpextrq r14, xmm0, 0       ; Extract the lower 64 bits (1 element) from xmm0 into r14
+    add rax, r14               ; Add r14 to rax
+    add r12, 8                ; Increment r12 by 8 (64 bits) to point to the next element in rdx
+    add r13, 8                ; Increment r13 by 8 (64 bits) to point to the next element in r8
+    dec rcx
+    jnz dotProduct_loop
 
-   dotProduct_exit:
-        vextracti128 xmm0, ymm2, 1 
-        vpaddq   xmm2, xmm2, xmm0  
-        vpermilps xmm1, xmm2, 0xE  
-        vpaddq   xmm2, xmm2, xmm1  
-        movq     qword [rsp], xmm2  
-        mov      rax, qword [rsp]   
-        mov      qword [r9], rax    
-        add      rsp, 8
-        pop      rbp
-        ret
+dotProduct_exit:
+    mov [r9],rax
+    ret
